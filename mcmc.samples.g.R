a56236ad08118fc02de859492a748257fe1aa82c
SLOANUSER <SLOANUSER@C02VL0NXHV2V.tld>
2021-01-26 08:45:27 -0500

    bayesian gform

---
+++ mcmc.samples.g.R
@@ -0,0 +1,91 @@
+# bayesian g formula
+library(tidyverse)
+library(rstanarm)
+
+dat = read.csv("saerela.csv")[,-1]
+
+# covariate 1 model
+# get data in the right format to do one of these
+
+
+names(dat)[2:16] = paste(letters[1:5], rep(1:3, each = 5), sep="")
+
+
+formatted = datg %>% pivot_longer(c(!Y), names_to = c(".value", "set"),
+                                  names_pattern = "(.)(.)")
+head(formatted)
+
+formatted$id = rep(1:500, each = 3)
+
+f2 = formatted %>% group_by(id) %>% mutate(alag = lag(a), 
+                                           blag = lag(b),
+                                           clag = lag(c),
+                                           dlag = lag(d),
+                                           elag = lag(e),
+                                           zlag = lag(Z)) %>% ungroup() %>% filter(set > 1)
+
+#amod = stan_glmer(a ~ (1|id) + alag + zlag, data = f2, family = gaussian())
+#bmod = stan_glmer(b ~ (1|id) + blag + zlag, data = f2, family = gaussian())
+#cmod = stan_glmer(c ~ (1|id) + clag + zlag, data = f2, family = gaussian())
+#dmod = stan_glmer(d ~ (1|id) + dlag + zlag, data = f2, family = gaussian())
+#emod = stan_glmer(e ~ (1|id) + elag + zlag, data = f2, family = gaussian())
+
+# outcome model
+dat$treat = with(dat, Z1 + Z2 + Z3)
+ymod = stan_glm(Y ~ a3 + b3 + c3 + d3 + e3 + treat, data = dat, family = binomial())
+
+
+t.model = c(1, 0)
+
+outcomes = array
+
+for (it in 1:nIter) {
+#it = 1
+# sample a parameter, then a covariate, 
+
+# sample the always treat trajectory
+# for subject 1
+
+# sample covariates 
+# relevant data (intercept, a1 z1)
+pmod = 3
+a2.treat = (rnorm(n, mean = as.matrix(cbind(1, select(dat, a1), Z1 = 1)) %*% as.matrix(amod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(amod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+b2.treat = (rnorm(n, mean = as.matrix(cbind(1, select(dat, b1), Z1 = 1)) %*% as.matrix(bmod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(bmod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+c2.treat = (rnorm(n, mean = as.matrix(cbind(1, select(dat, c1), Z1 = 1)) %*% as.matrix(cmod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(cmod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+d2.treat = (rnorm(n, mean = as.matrix(cbind(1, select(dat, d1), Z1 = 1)) %*% as.matrix(dmod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(dmod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+e2.treat = (rnorm(n, mean = as.matrix(cbind(1, select(dat, e1), Z1 = 1)) %*% as.matrix(emod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(emod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+
+a3.treat = (rnorm(n, mean = as.matrix(cbind(1, a2.treat, Z2 = 1)) %*% as.matrix(amod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(amod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+b3.treat = (rnorm(n, mean = as.matrix(cbind(1, b2.treat, Z2 = 1)) %*% as.matrix(bmod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(bmod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+c3.treat = (rnorm(n, mean = as.matrix(cbind(1, c2.treat, Z2 = 1)) %*% as.matrix(cmod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(cmod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+d3.treat = (rnorm(n, mean = as.matrix(cbind(1, d2.treat, Z2 = 1)) %*% as.matrix(dmod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(dmod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+e3.treat = (rnorm(n, mean = as.matrix(cbind(1, e2.treat, Z2 = 1)) %*% as.matrix(emod)[it, 1:3], sd = 0.25)) +
+  rnorm(n, mean = as.matrix(emod)[it, ((pmod+1):(pmod+n))], sd = 0.1 )
+
+
+y.outcome = as.matrix(cbind(1, a3.treat, b3.treat, c3.treat, d3.treat, e3.treat, treat=3)) %*% as.matrix(ymod)[it,]
+y.bin = rbinom(n, 1, prob = plogis(y.outcome))
+
+}
+
+formatted$id = rep(1:nrow(datg), each = 3) # to-do.. this automatically (using regex)
+formatted$set = as.numeric(formatted$set) - 1
+head(formatted)
